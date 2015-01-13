07c0412bdd232c49d2de6294ef4eb019c3f862fc
xcalatrava <xav@MacBook-de-xavi.local>
2015-01-14 00:35:47 +0100

    efecto descrubris cartas

--- js/script.js
+++ js/script.js
@@ -54,7 +54,28 @@ function Loadimages(callback) {
         carta18: '/images/18.jpg',
         carta19: '/images/19.jpg',
         carta20: '/images/20.jpg',
-        carta21: '/images/21.jpg',
+        carta21: '/images/21.jpg',        
+        carta1r: '/images/01r.jpg',
+        carta2r: '/images/02r.jpg',
+        carta3r: '/images/03r.jpg',
+        carta4r: '/images/04r.jpg',
+        carta5r: '/images/05r.jpg',
+        carta6r: '/images/06r.jpg',
+        carta7r: '/images/07r.jpg',
+        carta8r: '/images/08r.jpg',
+        carta9r: '/images/09r.jpg',
+        carta10r: '/images/10r.jpg',
+        carta11r: '/images/11r.jpg',
+        carta12r: '/images/12r.jpg',
+        carta13r: '/images/13r.jpg',
+        carta14r: '/images/14r.jpg',
+        carta15r: '/images/15r.jpg',
+        carta16r: '/images/16r.jpg',
+        carta17r: '/images/17r.jpg',
+        carta18r: '/images/18r.jpg',
+        carta19r: '/images/19r.jpg',
+        carta20r: '/images/20r.jpg',
+        carta21r: '/images/21r.jpg',
     };
 
 
@@ -77,31 +98,102 @@ function Loadimages(callback) {
 }
 
 function Muestra_cartas(images) {
-    //animacion para mostrar carta escogida 1
+
+    group_baraja.visible(false);
+    group_baraja.destroy();
+
     var cartas = group_escogidas.find('Image');
 
-    var tween = new Kinetic.Tween({
-        node: cartas[0],
-        duration: 1,
-        scaleX: 0
-    });
+    var escala = (stage.getWidth() - 20)/2;
 
-    tween.play();
+    escala = escala / cartas[0].width();
 
     var imageObj = new Image();
 
-    var imageObj = images[cartas[0].id()];
-    cartas[0].setImage(imageObj);
 
-    var tween = new Kinetic.Tween({
-        node: cartas[0],
-        duration: 1,
-        scaleX: 1
-    });
 
-    tween.play();
+    //animacion para mostrar carta escogida 1
+
+    var imageObj = images[cartas[0].id()+"r"];
+        var imageObj1 = images[cartas[1].id()+"r"];
+
+
+
+    var tl = new TimelineLite();
+    tl.to(cartas[0], 1, {
+        kinetic: {
+            x: 0,
+            y: (stage.getHeight() - cartas[0].height() * escala) / 2,
+            scale: escala
+        },
+        ease: Power4.easeOut
+    })
+        .to(cartas[0], 1, {
+            kinetic: {
+                x: cartas[0].width() * escala,
+                scaleX: 0
+            },
+            onComplete: function() {
+                cartas[0].setImage(imageObj);
+            }
+        })
+        .to(cartas[0], 1, {
+            kinetic: {
+                scaleX: -escala
+            }
+        });
+
+    // var tl = new TimelineLite();
+    // tl.to(cartas[0], 1, {
+    //     kinetic: {
+    //         x: 0,
+    //         y: (stage.getHeight() - cartas[0].height() * escala) / 2,
+    //         scale: escala
+    //     },
+    //     ease: Power4.easeOut
+    // })
+    //     .to(cartas[0], 1, {
+    //         kinetic: {
+    //             x: cartas[0].width() * escala,
+    //             scaleX: 0
+    //         },
+    //         onComplete: function() {
+    //             cartas[0].setImage(imageObj);
+    //         }
+    //     })
+    //     .to(cartas[0], 1, {
+    //         kinetic: {
+    //             scaleX: -escala
+    //         }
+    //     });
 
     // animacion para mostrar carta escogida 2
+
+
+
+    var tl2 = new TimelineLite();
+    tl.to(cartas[1], 1, {
+        kinetic: {
+            x: cartas[1].width() * escala + 20,
+            y: (stage.getHeight() - cartas[1].height() * escala) / 2,
+            scale: escala
+        },
+        ease: Power4.easeOut
+    })
+        .to(cartas[1], 1, {
+            kinetic: {
+                x: (cartas[1].width() * escala * 2) + 20,
+                scaleX: 0
+            },
+            onComplete: function() {
+                cartas[1].setImage(imageObj1);
+            }
+        })
+        .to(cartas[1], 1, {
+            kinetic: {
+                scaleX: -escala
+            }
+        });
 }
 
 
