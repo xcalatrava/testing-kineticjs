f956d8e2b24ba5e8524b4fa510bc925d4da6d150
xcalatrava <xav@MacBook-de-xavi.local>
2015-06-21 20:57:47 +0200

    ++sql

--- js/script.js
+++ js/script.js
@@ -8,149 +8,56 @@ var localDB = null;
 
 document.addEventListener("deviceready", onDeviceReady, false);
  
-function onDeviceReady(){
-    //$("#status").append("</br>" +  "Comprobando DB.");
-    try {
-            if (!window.sqlitePlugin.openDatabase) {
-                //$("#status").append("</br>" +  "Error, su navegador no permite crear BBDD.");
-                alert("Error, su navegador no permite crear BBDD.");
-            }
-            else {
-                // localDB = window.openDatabase("ResultadosDB", "1.0", "ResultadosDB", 65536);
-                localDB = window.sqlitePlugin.openDatabase({name: "ResultadosDB", location: 2});
-                CheckDB();
-                 //                 $("#status").html(""); 
-            };
+function onDeviceReady() {
 
-        }
-    catch (exception) {
-        alert("Algo salió mal al inicial el programa. Excepción:" + exception.message);
-    }
+    localDB = window.sqlitePlugin.openDatabase({
+        name: "ResultadosDB",
+        location: 2
+    });
 
-}
+    var query = null;
+    localDB.transaction(function(tx) {
+        query = 'DROP TABLE IF EXISTS info';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
+        query = 'DROP TABLE IF EXISTS predicciones';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
+        query = 'DROP TABLE IF EXISTS historico';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
 
-function CheckDBError(error)
-{
-    InicializaDB();
-    
-}
- 
-function CheckDBSuccess()
-{
-    $("#status").append("</br>" +  "La tabla existe");
-}
-
-//Comprueba si la DB existe
-function CheckDB() 
-{
-            localDB.transaction(function(tx) {            
-              tx.executeSql("SELECT * FROM info WHERE parametro = 'version'", [], function(tx, results) {
-                if (results.rows.length > 0){
-                    var dbversion = results.rows.item(0).valor;
-                }
-                            var last_version = $.getResults("aurictarot.json");
-                            if (last_version['version'] === dbversion) {
-                                alert("Las versiones son iguales.");
-                                return;
-                            }else{
-                                InicializaDB();
-                            }
-              });
-            },CheckDBError);
-            // Si las versiones no son iguales o no existe la tabla -> dbversion = null entonces inicializa la DB
-
-
-};
-function CreaTablas_OnSuccess(tx, results, query){
-    // $("#status").append("</br>" + tx);
-}
-
-function CreaTablas_OnError(Err){
-    alert("Error al crear las tablas:" + Err.message);
-    $("#status").append("</br>" + Err.message);
-}
-
-function CreaTablas(){
-        var query = null;
-        localDB.transaction(function(tx){
-                query = 'DROP TABLE IF EXISTS info';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);
-                query = 'DROP TABLE IF EXISTS predicciones';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);
-                query = 'DROP TABLE IF EXISTS historico';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);
-
-                query = 'CREATE TABLE IF NOT EXISTS info (id , parametro , valor )';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);
-                query = 'CREATE TABLE IF NOT EXISTS predicciones (combinacion , amor, trabajo )';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);
-                query = 'CREATE TABLE IF NOT EXISTS historico (fecha , pregunta , prediccion , puntuacion )';
-                tx.executeSql(query, [],CreaTablas_OnSuccess,CreaTablas_OnError);  
-            },CreaTablas_OnError);
-
-}
-
-
-function InsertaValores_onSuccess(tx,results){
-    // $("#status").append("</br>" +  "valor insertado.");
-}
-function InsertaValores_onError(Error){
-    $("#status").append("</br>" +  "Error al insertar:" + Error.message);
-}
-function InsertaValoresP_onSuccess(tx,results){
-    // $("#status").append("</br>" +  "valores insertados en predicciones.");
-}
-function InsertaValoresP_onError(Error){
-    $("#status").append("</br>" +  "Error al insertar en predicciones:" + Error.message);
-}
-function InsertaValores(){
+        query = 'CREATE TABLE IF NOT EXISTS info (id , parametro , valor )';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
+        query = 'CREATE TABLE IF NOT EXISTS predicciones (combinacion , amor, trabajo )';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
+        query = 'CREATE TABLE IF NOT EXISTS historico (fecha , pregunta , prediccion , puntuacion )';
+        tx.executeSql(query, [], CreaTablas_OnSuccess, SQL_OnError);
+    }, SQL_OnError);
     var field = null;
     var res = $.getResults("resultados.txt");
     var array_resultados = res.split("\n");
 
-    localDB.transaction(function(tx){
+    localDB.transaction(function(tx) {
 
-        for (var i = 0; i < array_resultados.length -1; i++) {
+        for (var i = 0; i < array_resultados.length - 1; i++) {
 
             field = array_resultados[i].split("':'");
-            field[0]=field[0].replace('\'','');
-            field[1]=field[1].replace('\'','');
-            field[2]=field[2].replace('\'','');
-            tx.executeSql("INSERT INTO predicciones (combinacion, amor, trabajo) VALUES (?,?,?)", 
-            [field[0], field[1], field[2]],
-            InsertaValoresP_onSuccess,
-            InsertaValoresP_onError);
+            field[0] = field[0].replace('\'', '');
+            field[1] = field[1].replace('\'', '');
+            field[2] = field[2].replace('\'', '');
+            tx.executeSql("INSERT INTO predicciones (combinacion, amor, trabajo) VALUES (?,?,?)", [field[0], field[1], field[2]],
+                SQL_OnError,
+                SQL_OnError);
         };
 
-            tx.executeSql('INSERT INTO info VALUES(1, "version", "1.0.0.1")', 
-            [], 
-            InsertaValores_onSuccess,
-            InsertaValores_onError);
-    });        
-     
+        tx.executeSql('INSERT INTO info VALUES(1, "version", "1.0.0.1")', [],
+            SQL_OnError,
+            SQL_OnError);
+    });
+
+    console.log("ya esta");
+
 
 }
 
-function InicializaDB(){
-
-    CreaTablas();
-
-    InsertaValores();
-    console.log("ya esta");  
-
-//    $("#status").html(""); 
-}
- 
- 
-
-function CreateTablesOK (tx, error) {
- //    $("#status").append("</br>" +  "Tabla creada correctamente.");
-}
-
-function CreateTablesError (tx, error){
-//    $("#status").append("</br>" +  "Error al crear tabla." + error.descripcion);
-
-}
 
 function Recupera_prediccion (){
            localDB.transaction(function(tx) {            
@@ -170,8 +77,12 @@ function Recupera_prediccion (){
                 }
 
               });
-            },CheckDBError);
+            },SQL_OnError);
            
+}
+
+function SQL_OnError (){
+    alert ("Error en SQL");
 }
 
 
